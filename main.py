f5a1317516e01a01178ef241a7ae76342530fd94
Samuel Berman <rocketberman@yahoo.com>
2017-09-20 21:15:58 -0700

    Add array

--- main.py
+++ main.py
@@ -2,9 +2,22 @@ from pytesseract import *
 from PIL import Image
 import os
 
+word = "hippo"
+
+# load image and convert to string
 img = Image.open(r'Test_Images\test2.png')
 text = image_to_string(img)
-text = '\n'.join([x for x in text.split("\n") if x.strip()!=''])
 
-with open("Output.txt", "w") as text_file:
-    print(text, file=text_file)
+def string_to_array(string):
+    string = '\n'.join([x for x in string.split("\n") if x.strip()!=''])
+    array = []
+    lines = string.split('\n')
+    
+    for line in lines:
+        array.append(list(line))
+
+    return array
+
+word_array = string_to_array(text)
+
+print(word_array)
